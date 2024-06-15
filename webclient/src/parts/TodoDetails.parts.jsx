6161725ab9d4b36ed47fbe8ed28758ff59dae9da
Rishav <111266613+rissshaaav@users.noreply.github.com>
2024-06-15 17:05:18 +0530

    chore: Refactor TodoDetails component to improve code readability and organization

--- webclient/src/parts/TodoDetails.parts.jsx
+++ webclient/src/parts/TodoDetails.parts.jsx
@@ -5,15 +5,21 @@ import updateTodo from "../services/todoRelated/updateTodo.service";
 import newTodo from "../services/todoRelated/newTodo.service";
 
 const TodoDetails = () => {
+    // State to store the todo details
     const [todoDetail, setTodoDetail] = useState({
         title: "",
         description: "",
         dueDate: "",
     });
+
+    // Get the todoId from the URL
     const params = useParams();
     const todoId = params.id;
+
+    // Fetch the todo details
     useEffect(() => {
         const fetchTodoDetails = async () => {
+            // If the todoId is "new", means user clicked on new todo button, then set the todoDetail to default
             if (todoId === "new") {
                 setTodoDetail({
                     title: "",
@@ -22,6 +28,7 @@ const TodoDetails = () => {
                 });
                 return;
             }
+            // Fetch the todo details by calling the todoDetails service with the todoId
             const data = await todoDetails(todoId);
             if (data) {
                 setTodoDetail(data);
@@ -29,21 +36,30 @@ const TodoDetails = () => {
         };
         fetchTodoDetails();
     }, [todoId]);
+
+    // Update the todo details or create a new todo
     useEffect(() => {
+        // function to update a todo or create a new todo based on the todoId
         const updateTodoDetails = async () => {
+            // If the todoId is "new", means user clicked on new todo button,
+            // then create a new todo by calling the newTodo service
             if (todoId === "new") {
                 await newTodo(todoDetail);
-            } else {
+            }
+            // Else update the todo by calling the updateTodo service
+            else {
                 await updateTodo(todoDetail);
             }
         };
-
+        // Set a timeout to update the todo details after 1.5 seconds of inactivity
         const timeoutId = setTimeout(updateTodoDetails, 1500);
-
+        // Clear the timeout if the component is unmounted
         return () => clearTimeout(timeoutId);
     }, [todoDetail, todoId]);
     return (
+        // Todo Details Container
         <div className="h-full p-2.5 border-[2px] border-borderColor dark:border-borderColorDark rounded-[10px] bg-background dark:bg-foregroundDark ">
+            {/* Todo Title */}
             <textarea
                 className="text-[30px] font-bold w-full h-min focus:outline-none dark:bg-foregroundDark"
                 placeholder="Enter Todo Title"
@@ -52,6 +68,7 @@ const TodoDetails = () => {
                     setTodoDetail({ ...todoDetail, title: e.target.value })
                 }
             ></textarea>
+            {/* Due Date */}
             <p className="text-[18px] font-semibold">
                 <span className="text-[#9ca3af]">Due Date: </span>
                 <span>
@@ -72,6 +89,7 @@ const TodoDetails = () => {
                     )}
                 </span>
             </p>
+            {/* Todo Description */}
             <textarea
                 className="w-full h-max focus:outline-none text-[18px] font-normal mt-2 dark:bg-foregroundDark"
                 placeholder="Enter Todo Description"
